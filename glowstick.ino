780454103a4ea795e62c4b815374a1dc17a7ec76
paupauline16 <maierpauline@gmx.de>
2025-11-11 19:52:57 +0800

    glowstick.ino

--- glowstick.ino
+++ glowstick.ino
@@ -3,7 +3,7 @@ int pinG = 10;
 int pinB = 9;
 int button = 2;
 
-int mode = 0; // 0=constant, 1=blink, 2=breathing
+int mode = 0; 
 int colorIndex = 0;
 
 unsigned long buttonTime = 0;
@@ -11,14 +11,14 @@ bool buttonPressed = false;
 unsigned long lastChange = 0;
 bool ledOn = true;
 
-// Breathing variables
+
 int brightness = 0;
-int step = 3;               // Helligkeitsschritt
+int step = 3;               
 bool increasing = true;
 unsigned long lastBreathUpdate = 0; 
-const unsigned long breathInterval = 20; // ms zwischen Helligkeitsschritten
+const unsigned long breathInterval = 20; 
 
-// Farben (R,G,B)
+
 int colors[][3] = {
   {255,255,255},
   {255,0,0},
@@ -42,7 +42,7 @@ void loop() {
   unsigned long now = millis();
   bool reading = digitalRead(button);
 
-  // Button check
+
   if (reading == LOW && !buttonPressed) {
     buttonPressed = true;
     buttonTime = now;
@@ -52,17 +52,15 @@ void loop() {
     buttonPressed = false;
 
     if (pressLength > 800) {
-      mode = (mode + 1) % 3; // long press -> change mode
+      mode = (mode + 1) % 3; 
       Serial.print("Mode: "); Serial.println(mode);
     } else {
-      colorIndex = (colorIndex + 1) % colorCount; // short press -> change color
+      colorIndex = (colorIndex + 1) % colorCount;
       Serial.print("Color: "); Serial.println(colorIndex);
     }
   }
 
-  // Run current mode
   if (mode == 0) {
-    // constant light
     setColor(colors[colorIndex]);
   }
   else if (mode == 1) {
@@ -75,7 +73,6 @@ void loop() {
     else setColorOff();
   }
   else if (mode == 2) {
-    // breathing (zeitbasiert)
     if (now - lastBreathUpdate > breathInterval) {
       lastBreathUpdate = now;
 
@@ -96,14 +93,12 @@ void loop() {
   }
 }
 
-// set LED color directly
 void setColor(int col[3]) {
   analogWrite(pinR, col[0]);
   analogWrite(pinG, col[1]);
   analogWrite(pinB, col[2]);
 }
 
-// turn off LEDs
 void setColorOff() {
   analogWrite(pinR, 0);
   analogWrite(pinG, 0);
